0bc0af94d567b3b05901a74ca048235e160ea08c
Shaun T Martin <webmaster@ginblades.com>
2017-07-14 21:31:16 -0500

    Generate token entity.

---
+++ src/AppBundle/Entity/ApiToken.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * ApiToken
+ *
+ * @ORM\Table(name="api_token")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\ApiTokenRepository")
+ */
+class ApiToken
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="string", length=100)
+     */
+    private $token;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="text")
+     */
+    private $notes;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set token
+     *
+     * @param string $token
+     */
+    public function setToken($token)
+    {
+        $this->token = $token;
+    }
+
+    /**
+     * Get token
+     *
+     * @return string
+     */
+    public function getToken()
+    {
+        return $this->token;
+    }
+
+    /**
+     * Set notes
+     *
+     * @param string $notes
+     */
+    public function setNotes($notes)
+    {
+        $this->notes = $notes;
+    }
+
+    /**
+     * Get notes
+     *
+     * @return string
+     */
+    public function getNotes()
+    {
+        return $this->notes;
+    }
+}
