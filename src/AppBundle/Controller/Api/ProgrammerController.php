d842d4813adfdf38f2970879343d3a3660c0f9aa
Shaun T Martin <webmaster@ginblades.com>
2017-07-17 21:23:16 -0500

    Did form type choices switch key/value? Or something is weird?

--- src/AppBundle/Controller/Api/ProgrammerController.php
+++ src/AppBundle/Controller/Api/ProgrammerController.php
@@ -4,8 +4,10 @@ namespace AppBundle\Controller\Api;
 
 use AppBundle\Controller\BaseController;
 use AppBundle\Entity\Programmer;
+use AppBundle\Form\ProgrammerType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\Form\FormInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -17,11 +19,14 @@ class ProgrammerController extends BaseController
      */
     public function newAction(Request $request)
     {
-        // second argument to 'json_decode' makes array instead of class
-        $data = json_decode($request->getContent(), true);
+        $programmer = new Programmer();
+        $form = $this->createForm(ProgrammerType::class, $programmer);
+        $this->processForm($request, $form);
 
-        $programmer = new Programmer($data['nickname'], $data['avatarNumber']);
-        $programmer->setTagLine($data['tagLine']);
+        if (!$form->isValid()) {
+            return new Response("It failed");
+        }
+
         $programmer->setUser($this->findUserByUsername('weaverryan'));
 
         $em = $this->getDoctrine()->getManager();
@@ -29,5 +34,11 @@ class ProgrammerController extends BaseController
         $em->flush();
 
         return new Response('It Worked!');
+    }
+
+    private function processForm(Request $request, FormInterface $form)
+    {
+        $data = json_decode($request->getContent(), true);
+        $form->submit($data);
     }
 }
--- src/AppBundle/Form/ProgrammerType.php
+++ src/AppBundle/Form/ProgrammerType.php
@@ -3,6 +3,10 @@
 namespace AppBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -11,24 +15,26 @@ class ProgrammerType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('nickname', 'text')
-            ->add('avatarNumber', 'choice', [
+            ->add('nickname', TextType::class)
+            // ->add('avatarNumber', NumberType::class)
+            ->add('avatarNumber', ChoiceType::class, [
                 'choices' => [
-                    1 => 'Girl (green)',
-                    2 => 'Boy',
-                    3 => 'Cat',
-                    4 => 'Boy with Hat',
-                    5 => 'Happy Robot',
-                    6 => 'Girl (purple)'
+                    'Girl (green)' => 1,
+                    'Boy' => 2,
+                    'Cat' => 3,
+                    'Boy with Hat' => 4,
+                    'Happy Robot' => 5,
+                    'Girl (purple)' => 6
                 ]
             ])
-            ->add('tagLine', 'textarea');
+            ->add('tagLine', TextareaType::class);
     }
 
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
-            'data_class' => 'AppBundle\Entity\Programmer'
+            'data_class' => 'AppBundle\Entity\Programmer',
+            'csrf_protection' => false
         ]);
     }
 
